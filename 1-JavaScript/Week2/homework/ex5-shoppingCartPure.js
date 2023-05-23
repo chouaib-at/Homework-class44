601bb97a186fa20acc4795864ab24dce9be3e085
CHOUAIB <128982286+chouaib-at@users.noreply.github.com>
2023-05-23 18:14:30 +0200

    solution for ex

--- 1-JavaScript/Week2/homework/ex5-shoppingCartPure.js
+++ 1-JavaScript/Week2/homework/ex5-shoppingCartPure.js
@@ -16,8 +16,15 @@ it pure. Do the following:
 5. Confirm that you function passes the provided unit tests.
 ------------------------------------------------------------------------------*/
 // ! Function under test
-function addToShoppingCart(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+
+function addToShoppingCart(shoppingCart, item) {
+  const newShoppingCart = [...shoppingCart];
+  newShoppingCart.push(item);
+  // if shopping cart have more than three items
+  if (newShoppingCart.length > 3) {
+    newShoppingCart.shift();
+  }
+  return newShoppingCart;
 }
 
 // ! Test functions (plain vanilla JavaScript)
