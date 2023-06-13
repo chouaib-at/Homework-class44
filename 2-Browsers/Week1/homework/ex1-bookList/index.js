4036190fa821418cfefefad2d4b2ed0f64864517
CHOUAIB <128982286+chouaib-at@users.noreply.github.com>
2023-06-13 17:07:30 +0200

    chouaib Atrous/Browsers/week 1

--- 2-Browsers/Week1/homework/ex1-bookList/index.js
+++ 2-Browsers/Week1/homework/ex1-bookList/index.js
@@ -1,22 +1,3 @@
-//cspell: disable
-/*------------------------------------------------------------------------------
-Full description at: https://github.com/HackYourFuture/Homework/tree/main/2-Browsers/Week1#exercise-1-the-book-list
-
-I'd like to display my three favorite books inside a nice webpage!
-
-1. Iterate through the array of books.
-2. For each book, create a `<p>`
-element with the book title and author.
-3. Use a `<ul>`  and `<li>` to display the books.
-4. Add an `<img>` to each book that links to a URL of the book cover.
-5. Change the style of the book depending on whether you have read it(green) or not(red).
-
-The end result should look something like this:
-https://hackyourfuture.github.io/example-pages/Browsers/Week1/1-booklist/
-
------------------------------------------------------------------------------*/
-//cspell: enable
-
 function createBookList(books) {
   const ul = document.createElement('ul');
   for (let i = 0; i < books.length; i++) {
