55deb22c88fb0406787b086decd03a06f4ae3916
CHOUAIB <128982286+chouaib-at@users.noreply.github.com>
2023-07-03 17:50:33 +0200

    homework API W1

--- 3-UsingAPIs/Week1/homework/ex3-rollDie.js
+++ 3-UsingAPIs/Week1/homework/ex3-rollDie.js
@@ -1,62 +1,43 @@
 'use strict';
-/*------------------------------------------------------------------------------
-Full description at: https://github.com/HackYourFuture/Homework/tree/main/3-UsingAPIs/Week1#exercise-3-roll-a-die
 
-- Run the unmodified program and confirm that problem described occurs.
-- Refactor the `rollDie()` function from callback-based to returning a
-  promise.
-- Change the calls to `callback()` to calls to `resolve()` and `reject()`.
-- Refactor the code that call `rollDie()` to use the promise it returns.
-- Does the problem described above still occur? If not, what would be your
-  explanation? Add your answer as a comment to be bottom of the file.
-------------------------------------------------------------------------------*/
+function rollDie() {
+  return new Promise((resolve, reject) => {
+    const randomRollsToDo = Math.floor(Math.random() * 8) + 3;
+    console.log(`Die scheduled for ${randomRollsToDo} rolls...`);
 
-// TODO Remove callback and return a promise
-function rollDie(callback) {
-  // Compute a random number of rolls (3-10) that the die MUST complete
-  const randomRollsToDo = Math.floor(Math.random() * 8) + 3;
-  console.log(`Die scheduled for ${randomRollsToDo} rolls...`);
+    const rollOnce = (roll) => {
+      const value = Math.floor(Math.random() * 6) + 1;
+      console.log(`Die value is now: ${value}`);
 
-  const rollOnce = (roll) => {
-    // Compute a random die value for the current roll
-    const value = Math.floor(Math.random() * 6) + 1;
-    console.log(`Die value is now: ${value}`);
+      if (roll > 6) {
+        reject(new Error('Oops... Die rolled off the table.'));
+        return;
+      }
 
-    // Use callback to notify that the die rolled off the table after 6 rolls
-    if (roll > 6) {
-      // TODO replace "error" callback
-      callback(new Error('Oops... Die rolled off the table.'));
-    }
+      if (roll === randomRollsToDo) {
+        resolve(value);
+        return;
+      }
 
-    // Use callback to communicate the final die value once finished rolling
-    if (roll === randomRollsToDo) {
-      // TODO replace "success" callback
-      callback(null, value);
-    }
-
-    // Schedule the next roll todo until no more rolls to do
-    if (roll < randomRollsToDo) {
       setTimeout(() => rollOnce(roll + 1), 500);
-    }
-  };
+    };
 
-  // Start the initial roll
-  rollOnce(1);
-}
-
-function main() {
-  // TODO Refactor to use promise
-  rollDie((error, value) => {
-    if (error !== null) {
-      console.log(error.message);
-    } else {
-      console.log(`Success! Die settled on ${value}.`);
-    }
+    rollOnce(1);
   });
 }
 
-// ! Do not change or remove the code below
+function main() {
+  rollDie()
+    .then((value) => {
+      console.log(`Success! Die settled on ${value}.`);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
 if (process.env.NODE_ENV !== 'test') {
   main();
 }
+
 module.exports = rollDie;
